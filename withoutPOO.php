b6ba70cc223c3a4b3ffa5fe6d7aa59378e726823
EricB-59 <151823587+EricB-59@users.noreply.github.com>
2024-10-09 20:05:32 +0200

    eric

--- withoutPOO.php
+++ withoutPOO.php
@@ -1,17 +1,19 @@
 <?php
+
+//  --------------- [Start testing bank account #1, No overdraft] ----------------
 $bankAccount1 = 400;
 $bankActivity1 = true;
 $bankOverdraft1 = false;
 
 echo "My balance : ".$bankAccount1."<br>";
 
-$bankActivity1 = true;
-echo "My account is now ";
-echo $bankActivity1 ? "closed.<br>" : "reopened.<br>";
-
 $bankActivity1 = false;
 echo "My account is now ";
-echo $bankActivity1 ? "closed.<br>" : "reopened.<br>";
+echo $bankActivity1 ? "reopened.<br>" : "closed.<br>";
+
+$bankActivity1 = true;
+echo "My account is now ";
+echo $bankActivity1 ? "reopened.<br>" : "closed.<br>";
 
 echo "Doing transaction deposit (+150) with current balance ".$bankAccount1."<br>";
 $bankAccount1 += 150;
@@ -29,3 +31,56 @@ if (!$bankOverdraft1 && ($bankAccount1 - 600) <= 0) {
 
 $bankActivity1 = false;
 echo "My account is now closed.<br>";
+
+// --------------- [Start testing bank account #2, Silver overdraft (100.0 founds)] ----------------
+echo "<br>";
+$bankAccount2 = 200;
+$bankActivity2 = true;
+$bankOverdraft2 = true;
+$bankOverdraftLimit2 = 100;
+
+echo "My balance : ".$bankAccount2."<br>";
+
+echo "Doing transaction deposit (+100) with current balance ".$bankAccount2."<br>";
+$bankAccount2 += 100;
+echo "My new balance after the deposit (+100) : ".$bankAccount2."<br>";
+
+echo "Doing transaction withdrawl (-300) with current balance ".$bankAccount2."<br>";
+$bankAccount2 -= 300;
+echo "My new balance after the withdrawl (-300) : ".$bankAccount2."<br>";
+
+echo "Doing transaction withdrawl (-50) with current balance ".$bankAccount2."<br>";
+if (($bankOverdraftLimit2 - 50) >= 100) {
+    echo "My balance after failed last transaction : -50";
+    echo "Error transaction: withdrawl exceeds overdraft limit.<br>";
+}else {
+    $bankAccount2 -= 50;
+    $bankOverdraftLimit2 -= 50;
+    echo "My new balance after the withdrawl (-50) : ".$bankAccount2."<br>";
+}
+
+echo "Doing transaction withdrawl (-120) with current balance ".$bankAccount2."<br>";
+
+if (($bankOverdraftLimit2 - 120) <= $bankOverdraftLimit2) {
+    echo "Error transaction: withdrawl exceeds overdraft limit.<br>";
+    echo "My balance after failed last transaction : $bankAccount2<br>";
+}else {
+    $bankAccount2 -= 120;
+    echo "My new balance after the withdrawl (-120) : ".$bankAccount2."<br>";
+}
+
+echo "Doing transaction withdrawl (-20) with current balance ".$bankAccount2."<br>";
+if (($bankOverdraftLimit2 - 20) <= 0) {
+    echo "My balance after failed last transaction : -20";
+    echo "Error transaction: withdrawl exceeds overdraft limit.<br>";
+}else {
+    $bankAccount2 -= 20;
+    echo "My new balance after the withdrawl (-20) : ".$bankAccount2."<br>";
+}
+
+$bankActivity2 = false;
+echo "My account is now ";
+echo $bankActivity2 ? "reopened.<br>" : "closed.<br>";
+
+echo "Error: Account is alredy ";
+echo $bankActivity2 ? "reopened.<br>" : "closed.<br>";
